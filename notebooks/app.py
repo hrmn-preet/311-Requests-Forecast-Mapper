052df81eaf19904f4e5f0bc3f4fcadcff49da1bd
Harmanpreet Kaur <43276428+hrmn-preet@users.noreply.github.com>
2024-08-06 01:31:35 -0600

    Update app.py

--- notebooks/app.py
+++ notebooks/app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 st.title('Upload and Display GIF')
 
-# File uploader allows you to upload files
+# File uploader 
 uploaded_file = st.file_uploader("Choose a GIF file", type="gif")
 
 if uploaded_file is not None:
     # Display the GIF file
-    st.image(uploaded_file, format="gif")
+    st.image(uploaded_file) 
